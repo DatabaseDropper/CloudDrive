d2165e54e6a0e96a91dc1ab1c792b48adef991b9
DatabaseDropper <47637776+DatabaseDropper@users.noreply.github.com>
2020-03-22 14:48:55 +0100

    Sprzątanie oraz dodanie interfejsu IFileSystem

---
+++ src/CloudDrive/Interfaces/IFileSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudDrive.Interfaces
+{
+    interface IFileSystem
+    {
+        public (bool Success, string Error) TrySaveFile(string path, byte[] bytes);
+
+        public (bool Success, string Error, byte[] bytes) TryGetFile(string path);
+
+        public (bool Success, string Error) TryRemoveFile(string path);
+    }
+}
