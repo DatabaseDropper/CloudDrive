04962e20d3bfd5b3b454de4b02ec81ddad9806e9
hutyraprzemyslaw <63185347+hutyraprzemyslaw@users.noreply.github.com>
2020-04-11 19:08:18 +0200

    dzialajaca testowa baza

--- src/CloudDrive.Tests/CustomWebApplicationFactory.cs
+++ src/CloudDrive.Tests/CustomWebApplicationFactory.cs
@@ -1,11 +1,18 @@
 ﻿using CloudDrive.Tests;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 
 public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    protected override IHostBuilder CreateHostBuilder()
     {
-        builder.UseStartup<FakeStartup>();
+        var builder = Host.CreateDefaultBuilder()
+                          .ConfigureWebHostDefaults(x =>
+                          {
+                              x.UseStartup<FakeStartup>().UseTestServer();
+                          });
+        return builder;
     }
 }
