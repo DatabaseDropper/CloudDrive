d2e162b884e9ce8b92fcc200de6b94550a99cf21
hutyraprzemyslaw <63185347+hutyraprzemyslaw@users.noreply.github.com>
2020-04-11 14:47:01 +0200

    Encja dysku

---
+++ src/CloudDrive/Models/Disk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CloudDrive.Models
+{
+	public class Disk
+	{
+		public Guid Id { get; private set; } = Guid.NewGuid();
+
+		public string Name { get; set; }
+
+		public Guid FolderId { get; private set; }
+
+		public Folder Folder { get; private set; }
+
+		public long TotalSpaceAsKB { get; private set; }
+
+		public long UsedSpaceAsKB { get; private set; }
+
+		public long FreeSpaceAsKB => TotalSpaceAsKB - UsedSpaceAsKB;
+
+		public DateTime CreationDate { get; private set; } = DateTime.Now;
+
+		public void ResizeDisk(long DesiredSize)
+		{
+			if (DesiredSize < 0)
+				throw new Exception("Desired disk size must be greaeter or equal to 0");
+
+			TotalSpaceAsKB = DesiredSize;
+		}
+	}
+}
