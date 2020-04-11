69db636e720866fd89c15546b7ad2d2f8ff921e0
hutyraprzemyslaw <63185347+hutyraprzemyslaw@users.noreply.github.com>
2020-04-11 16:14:18 +0200

    Database Context

---
+++ src/CloudDrive/Database/Context.cs
@@ -0,0 +1,28 @@
+﻿using CloudDrive.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CloudDrive.Database
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Disk> Disks { get; set; }
+
+        public DbSet<File> Files { get; set; }
+
+        public DbSet<Folder> Folders { get; set; }
+    }
+}
