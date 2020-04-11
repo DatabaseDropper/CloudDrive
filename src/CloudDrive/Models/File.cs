67e52f472384a6d048fdfd84f89577e4fd15c3c7
hutyraprzemyslaw <63185347+hutyraprzemyslaw@users.noreply.github.com>
2020-04-11 15:04:42 +0200

    Encja File

---
+++ src/CloudDrive/Models/File.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CloudDrive.Models
+{
+    public class File
+    {
+        private File()
+        {
+
+        }
+
+        public File(string userFriendlyName, string physicalFileName, long sizeAsKB, Guid uploadedById, User uploadedBy)
+        {
+            UserFriendlyName = userFriendlyName;
+            PhysicalFileName = physicalFileName;
+            SizeAsKB = sizeAsKB;
+            UploadedById = uploadedById;
+            UploadedBy = uploadedBy;
+        }
+
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        public string UserFriendlyName { get; set; }
+
+        public string PhysicalFileName { get; set; }
+
+        public long SizeAsKB { get; private set; }
+
+        public bool IsAccessibleForEveryone { get; set; } = false;
+
+        public Guid UploadedById { get; private set; }
+
+        public User UploadedBy { get; private set; }
+
+        public DateTime UploadedAt { get; private set; } = DateTime.Now;
+    }
+}
