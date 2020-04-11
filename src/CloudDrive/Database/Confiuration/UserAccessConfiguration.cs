a1dc3ca69fc7c49a0aec4707c42cc067c21cab09
hutyraprzemyslaw <63185347+hutyraprzemyslaw@users.noreply.github.com>
2020-04-11 16:26:22 +0200

    Konfiguracja bazy - UserAccess

---
+++ src/CloudDrive/Database/Confiuration/UserAccessConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CloudDrive.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CloudDrive.Database.Confiuration
+{
+    public class UserAccessConfiguration : IEntityTypeConfiguration<UserAccess>
+    {
+        public void Configure(EntityTypeBuilder<UserAccess> builder)
+        {
+            builder.HasKey(x => new { x.UserId, x.FolderId });
+
+            builder.HasOne(x => x.Folder).WithMany(x => x.AuthorizedUsers).HasForeignKey(x => x.FolderId);
+
+            builder.HasOne(x => x.User).WithMany(x => x.SharedFolders).HasForeignKey(x => x.UserId);
+        }
+    }
+}
