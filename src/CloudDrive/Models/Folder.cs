2695964fa752be784e764750aa06ea0672bc0108
hutyraprzemyslaw <63185347+hutyraprzemyslaw@users.noreply.github.com>
2020-04-11 15:03:20 +0200

    Encja folder

---
+++ src/CloudDrive/Models/Folder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudDrive.Models
+{
+    public class Folder
+	{
+		public Guid Id { get; private set; } = Guid.NewGuid();
+
+		public string Name { get; set; } = "Folder";
+
+		public bool IsAccessibleForEveryone { get; set; } = false;
+
+		public List<File> Files { get; private set; } = new List<File>();
+
+		public List<Folder> Folders { get; private set; } = new List<Folder>();
+
+		public Guid? ParentFolderId { get; private set; }
+
+		public Folder ParentFolder { get; private set; }
+
+		public List<UserAccess> AuthorizedUsers { get; private set; } = new List<UserAccess>();
+
+		public DateTime CreationDate { get; private set; } = DateTime.Now;
+	}
+}
