69f86096b5d30fe6f57cb9c6bdede670730ef606
DatabaseDropper <47637776+DatabaseDropper@users.noreply.github.com>
2020-04-18 13:33:03 +0200

    Zmiana nazwy Account[Controllera]

--- src/CloudDrive/Controllers/AccountController.cs
+++ src/CloudDrive/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CloudDrive.Interfaces;
+using CloudDrive.Models.Input;
+using CloudDrive.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudDrive.Controllers
@@ -9,15 +11,18 @@ namespace CloudDrive.Controllers
     public class AccountController : ControllerBase
     {
         private readonly ITokenService _tokenService;
+        private readonly AccountService _accountService;
 
-        public AccountController(ITokenService tokenService)
+        public AccountController(ITokenService tokenService, AccountService accountService)
         {
             _tokenService = tokenService;
+            _accountService = accountService;
         }
 
         [HttpPost("Register")]
-        public IActionResult Register()
+        public IActionResult Register([FromBody] RegisterInput input)
         {
+            _accountService.TryRegister(input);
             return Ok(DateTime.Now);
         }
     }
