73714920c19f97d357d506f6354fa6f96e3b9e96
Zbysniew69 <62463918+zbysniew69@users.noreply.github.com>
2020-06-22 20:31:25 +0200

    Pobieranie informacji dot. konta

--- src/CloudDrive/Controllers/AccountController.cs
+++ src/CloudDrive/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using CloudDrive.Interfaces;
 using CloudDrive.Models.Input;
 using CloudDrive.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudDrive.Controllers
@@ -13,11 +14,13 @@ namespace CloudDrive.Controllers
     {
         private readonly ITokenService _tokenService;
         private readonly AccountService _accountService;
+        private readonly UserService _userService;
 
-        public AccountController(ITokenService tokenService, AccountService accountService)
+        public AccountController(ITokenService tokenService, AccountService accountService, UserService userService)
         {
             _tokenService = tokenService;
             _accountService = accountService;
+            _userService = userService;
         }
 
         [HttpPost("Register")]
@@ -34,6 +37,17 @@ namespace CloudDrive.Controllers
             var result = await _accountService.TrySignIn(input);
 
             return result.Success ? Ok(result.Data) : (IActionResult)BadRequest(result.Errors);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> AccountInfo()
+        {
+            var user = await _userService.TryGetUserAsync(UserId());
+
+            var result = await _accountService.ObtainAccountInfo(user);
+
+            return result.Success ? Ok(result.Data) : (IActionResult)BadRequest("Something went wrong");
         }
     }
 }
