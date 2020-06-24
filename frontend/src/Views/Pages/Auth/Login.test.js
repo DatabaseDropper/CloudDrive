d6397388e940311a52acd9b0e282c561fad4e861
kubagren <kubgre@gmail.com>
2020-06-24 16:46:54 +0200

    naprawienie testow logowania i rejestracji

--- frontend/src/Views/Pages/Auth/Login.test.js
+++ frontend/src/Views/Pages/Auth/Login.test.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import {
+    BrowserRouter as Router,
+} from 'react-router-dom';
 import '../../../Tests/Mocks/matchMedia.js';
 
 import Page_Auth_Login from './Login';
 
 test('Page Login - check is elements exists', () => {
-    const { container } = render(<Page_Auth_Login />);
+    const { container } = render(<Router><Page_Auth_Login /></Router>);
 
     expect(container.querySelector('[name="loginOrEmail"]')).toBeInTheDocument();
     expect(container.querySelector('[name="password"]')).toBeInTheDocument();
 });
 
 test('Page Login - check is password element has correct type', () => {
-    const { container } = render(<Page_Auth_Login />);
+    const { container } = render(<Router><Page_Auth_Login /></Router>);
 
     expect(container.querySelector('[name="password"]').getAttribute('type')).toBe('password');
 });
--- frontend/src/Views/Pages/Auth/Register.test.js
+++ frontend/src/Views/Pages/Auth/Register.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import {
+    BrowserRouter as Router,
+} from 'react-router-dom';
 import '../../../Tests/Mocks/matchMedia.js';
 
 import Page_Auth_Register from './Register';
 
 test('Page Register - check is elements exists', () => {
-    const { container } = render(<Page_Auth_Register />);
+    const { container } = render(<Router><Page_Auth_Register /></Router>);
 
     expect(container.querySelector('[name="login"]')).toBeInTheDocument();
     expect(container.querySelector('[name="password"]')).toBeInTheDocument();
@@ -14,7 +17,7 @@ test('Page Register - check is elements exists', () => {
 });
 
 test('Page Register - check is password element has correct type', () => {
-    const { container } = render(<Page_Auth_Register />);
+    const { container } = render(<Router><Page_Auth_Register /></Router>);
 
     expect(container.querySelector('[name="password"]').getAttribute('type')).toBe('password');
 });
