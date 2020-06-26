4fec014a938b1ab4f73c523045bbce63e51da0b7
m13bbb <broken.cord@gmail.com>
2020-06-26 17:20:45 +0200

    stworzenie funkcjonalności dodawania nowego folderu

--- frontend/src/Views/Pages/FolderAdd.js
+++ frontend/src/Views/Pages/FolderAdd.js
@@ -3,19 +3,69 @@ import {withRouter} from "react-router";
 
 import { Link } from 'react-router-dom';
 import { Form, Input, Button, Typography, Spin, Alert, Row, Col } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
+
+import Service_Api from './../../Service/Api';
 
 import Component_Text_Align from './../Components/Text/Align';
 
 import Layout_Auth from './../Layouts/Auth';
 import Component_Form_AbstractForm from './../Components/Form/AbstractForm';
 
+const defaultErrorMsg = 'Wystąpił błąd podczas dodawania folderu. Sprubuj jeszcze raz';
+
 class Page_FolderAdd extends Component_Form_AbstractForm {
+    handleSubmit() {
+        this.setState({ submitProcess: true });
+
+        Service_Api.fetch('/api/v1/Folder/' + this.props.match.params.folderId, {
+            data: {
+                method: 'POST',
+                body: JSON.stringify(this.state.fields),
+            },
+            onSuccess: (res) => {
+                if (200 == res.status) {
+                    
+                    res.json()
+                    .then((res) => {
+                        this.props.history.push('/folder/' + this.props.match.params.folderId);
+                    });
+                } else if(400 == res.status) {
+                    res.json()
+                    .then((res) => {
+                        console.log(res);
+                        let errors = [];
+
+                        for(const key in res.errors) {
+                            if (res.errors.hasOwnProperty(key)) {
+                                errors.push(res.errors[key]);
+                            }
+                        }
+
+                        this.setState({ submitProcess: false, errors: errors });
+                    })
+                    .catch(() => {
+                        this.setState({ submitProcess: false, errors: [defaultErrorMsg] });
+                    });
+                } else {
+                    this.setState({ submitProcess: false, errors: [defaultErrorMsg] });
+                }
+            },
+            onError: (error) => {
+                this.setState({ submitProcess: false, errors: [defaultErrorMsg] });
+            }
+        });
+    }
+    
     render() {
         return (
             <Layout_Auth>
-                <Row>
+                <Row gutter={[16, 16]}>
                     <Col span={24}>
-                        <Link to={("/folder/" + this.props.match.params.folderId)}></Link>
+                        <Link to={("/folder/" + this.props.match.params.folderId)}>
+                            <LeftOutlined />
+                            Wróć
+                        </Link>
                     </Col>
                 </Row>
                 <Row>
@@ -31,7 +81,7 @@ class Page_FolderAdd extends Component_Form_AbstractForm {
                             </div>
                         : null }
                         <Form onFinish={this.handleSubmit} layout="vertical">
-                            <Form.Item name="name" label="Nazwa folderu" required={false}>
+                            <Form.Item name="name" label="Nazwa folderu" required={true}>
                                 <Input name="name" onChange={this.handleChange} value={this.state.fields.name} />
                             </Form.Item>
                             <Component_Text_Align.Center>
@@ -40,7 +90,7 @@ class Page_FolderAdd extends Component_Form_AbstractForm {
                                     :
                                     <Form.Item>
                                         <Button type="primary" htmlType="submit">
-                                            Zaloguj się
+                                            Dodaj
                                         </Button>
                                     </Form.Item>
                                 }
