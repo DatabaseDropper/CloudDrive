daf0e543b4de951200a4232a3def5ff907f04933
m13bbb <broken.cord@gmail.com>
2020-06-26 14:16:12 +0200

    wywołanie akcji pobrania danych użytkownika po zalogowaniu / wejściu do aplikacji

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,38 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './Store';
+import { authSetAccountInfo } from './Actions';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Service_Api from './Service/Api';
+
+setInterval(() => {
+    if (store.getState().has('authData')) {
+        Service_Api.fetch('/api/v1/Account', {
+            data: {
+                method: 'GET',
+            },
+            onSuccess: (res) => {
+                if (200 == res.status) {
+                    res.json()
+                    .then((res) => {
+                        store.dispatch(authSetAccountInfo(res));
+                    });
+                }
+            }
+        });
+    }
+}, 5000);
+
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
